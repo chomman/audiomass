ab1f0c2919bf952ec23aa6d4dab58aee1f2bc70f
John Standish <voldron@gmail.com>
2017-01-09 14:16:24 -0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,192 @@
+var config = {
+	localDir:               './', //'./empty' ||
+	deleteRemoved:          true,
+	enableBucketCORS:       true,
+	ensureBucketWebsite:    true,
+	noBucketWebsiteRoutingRules: true,
+	bucketWebsite:          {
+		IndexDocument: {Suffix: 'index.html'},
+		ErrorDocument: {Key: 'index.html'},
+		RoutingRules:  [{
+			Condition: {HttpErrorCodeReturnedEquals: '404'},
+			Redirect:  {
+				HostName:             null, // will be filled in pragmatically, or you can override with your config.js
+				ReplaceKeyPrefixWith: '#!/', // Useful for AngularJS single page apps
+				HttpRedirectCode: '307', // Keeps Cloudfront from caching redirect responses, so if a file is missing you can re-upload
+				Protocol: 'https'
+			}
+		}]
+	},
+	s3Options:              {
+		ACL:             'public-read',
+		Bucket:          'audiomass-web',
+		accessKeyId:     process.env.AWS_ACCESS_KEY_ID,
+		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+		region:          'us-east-1',
+		Prefix:          '',
+		concurrency:     8
+	},
+	ensureDistribution:     true,
+	cloudfrontDistribution: {
+		DistributionConfig: {
+			Comment:              'Created by s3-site-sync', /* required -- can override with empty string/etc if desired */
+			DefaultCacheBehavior: { /* required */
+				ForwardedValues:      { /* required */
+					Cookies:     { /* required */
+						Forward:   'none' // /*, // required (none | whitelist | all) */
+						/*WhitelistedNames: { // optional
+							Quantity: 0, /!* required *!/
+							Items:    [ /!* more items *!/
+							]
+						}*/
+					},
+					QueryString: true /*, // required (true | false) */
+					/*Headers:     { //optionsl
+						Quantity: 0 /!*, // required *!/
+						/!*Items:    [
+							/!* more items *!/
+						]*!/
+					}*/
+				},
+				MinTTL:               0, /* required */
+				TargetOriginId:       'audiomass-web.s3-website-us-east-1.amazonaws.com', /* required, leave blank -- filled in
+                                                                                                      pragmatically */
+				TrustedSigners:       { /* required */
+					Enabled:  false, /* required (true || false) */
+					Quantity: 0, /* required */
+					Items:    [/* 'STRING_VALUE', 'more_items' */]
+				},
+				ViewerProtocolPolicy: 'redirect-to-https', /* required (allow-all | https-only | redirect-to-https) */
+				AllowedMethods:       {
+					Quantity:      3, /* required */
+					Items:         ['HEAD', 'GET', 'OPTIONS'], /* required (GET | HEAD | POST | PUT | PATCH | OPTIONS | DELETE) */
+					CachedMethods: {
+						Quantity: 3, /* required */
+						Items:    ['HEAD', 'GET', 'OPTIONS'] /* required (GET | HEAD | POST | PUT | PATCH | OPTIONS | DELETE) */
+					}
+				},
+				/*DefaultTTL:           0,
+				MaxTTL:               0, */
+				SmoothStreaming:      false // (true || false)
+			},
+			Enabled:              true, /* required (true || false) */
+			Origins:              { /* required */
+				Quantity: 1, /* required */
+				Items:    [
+					{
+						DomainName:         'audiomass-web.s3-website-us-east-1.amazonaws.com', /* required -- filled by module */
+						Id:                 'Custom-audiomass-web.s3-website-us-east-1.amazonaws.com, /* required -- filled by module */
+						CustomOriginConfig: {
+							HTTPPort:             80, /* required */
+							HTTPSPort:            443, /* required */
+							OriginProtocolPolicy: 'http-only' /* required (http-only | match-viewer) NOTE: HTTPS will NOT work w/ S3 bucket
+						                                          website!!*/
+						}
+						//, OriginPath:         'STRING_VALUE', // Needed if not root path
+						/*S3OriginConfig:     { // Needed if private bucket/objects
+							OriginAccessIdentity: 'STRING_VALUE' /!* required *!/
+						}*/
+					}
+				]
+			},
+			Aliases:              { // Provide in imported config if desired, necessary if pointing CNAME/etc records to cloudfront
+				Quantity: 0, /* required */
+				Items:    [ /* 'STRING_VALUE', 'another_value' */ ]
+			},
+			CacheBehaviors:       {
+				Quantity: 0, /* required */
+				Items:    [
+					/*{
+						ForwardedValues:      {
+							/!* required *!/
+							Cookies:     {
+								/!* required *!/
+								Forward:          'none | whitelist | all', /!* required *!/
+								WhitelistedNames: {
+									Quantity: 0, /!* required *!/
+									Items:    [
+										'STRING_VALUE',
+										/!* more items *!/
+									]
+								}
+							},
+							QueryString: true || false, /!* required *!/
+							Headers:     {
+								Quantity: 0, /!* required *!/
+								Items:    [
+									'STRING_VALUE',
+									/!* more items *!/
+								]
+							}
+						},
+						MinTTL:               0, /!* required *!/
+						PathPattern:          'STRING_VALUE', /!* required *!/
+						TargetOriginId:       'STRING_VALUE', /!* required *!/
+						TrustedSigners:       {
+							/!* required *!/
+							Enabled:  true || false, /!* required *!/
+							Quantity: 0, /!* required *!/
+							Items:    [
+								'STRING_VALUE',
+								/!* more items *!/
+							]
+						},
+						ViewerProtocolPolicy: 'allow-all | https-only | redirect-to-https', /!* required *!/
+						AllowedMethods:       {
+							Items:         [/!* required *!/
+								'GET | HEAD | POST | PUT | PATCH | OPTIONS | DELETE',
+								/!* more items *!/
+							],
+							Quantity:      0, /!* required *!/
+							CachedMethods: {
+								Items:    [/!* required *!/
+									'GET | HEAD | POST | PUT | PATCH | OPTIONS | DELETE',
+									/!* more items *!/
+								],
+								Quantity: 0 /!* required *!/
+							}
+						},
+						DefaultTTL:           0,
+						MaxTTL:               0,
+						SmoothStreaming:      true || false
+					}*/ /* more items */
+				]
+			},
+			CustomErrorResponses: {
+				Quantity: 0, /* required */
+				Items:    [
+					/*{
+						ErrorCode:          0, /!* required *!/
+						ErrorCachingMinTTL: 0,
+						ResponseCode:       'STRING_VALUE',
+						ResponsePagePath:   'STRING_VALUE'
+					}*/ /* more items */
+				]
+			},
+			DefaultRootObject:    'index.html',
+			Logging:              {
+				Bucket:         '', /* required */
+				Enabled:        false, /* required (true || false) */
+				IncludeCookies: false, /* required (true || false) */
+				Prefix:         '' /* required */
+			},
+			PriceClass:           'PriceClass_100', // USA/Europe (PriceClass_100 | PriceClass_200 | PriceClass_All)
+			Restrictions:         {
+				GeoRestriction: { /* required */
+					Quantity:        0, /* required */
+					RestrictionType: 'none', /* required (blacklist | whitelist | none) */
+					Items:           [/* 'STRING_VALUE', more items */]
+				}
+			},
+			ViewerCertificate:    {
+				CloudFrontDefaultCertificate: true // (true || false) -- provide in imported config if desired & set to false
+				// IAMCertificateId:             'iamCertId', // Your IAM SSL Certificate ID
+				// MinimumProtocolVersion:       'TLSv1', // 'SSLv3 | TLSv1',
+				// SSLSupportMethod:             'sni-only' // (sni-only | vip) -- VIP cost $$!!
+			}
+		}
+	},
+	ensureDistributionDefaultRootObj: false
+};
+
+module.exports = config;
