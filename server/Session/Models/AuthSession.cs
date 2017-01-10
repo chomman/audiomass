06bd737fd81c4a493bc98667b2de46ef21bcca06
John Standish <voldron@gmail.com>
2017-01-10 14:26:43 -0800

    Create AuthSession.cs

---
+++ server/Session/Models/AuthSession.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AudioMass.Session.Models
+{
+    public class AuthSession
+    {
+        public string AccountId { get; set; }
+    }
+}
